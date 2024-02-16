155ea4403be5ca2b46eb1ee28aea4299d36f3c24
Dani <perdan90@gmail.com>
2024-02-16 15:18:18 +0100

    feat(DNIValidator): add comprobations to check that the number is valid

--- src/main/java/cd/dperez/DNIValidator.java
+++ src/main/java/cd/dperez/DNIValidator.java
@@ -4,11 +4,40 @@ public class DNIValidator {
 
     private final DniLetterDictionary DNI_CORRESPONDING_LETTER = new DniLetterDictionary();
     private final int DNI_DIVISOR_NUM = 23;
+    private final int DNI_NUMBER_LENGTH = 8;
+    private boolean isValid = false;
     private String dniNumber = "";
     private int dniAlgorithmNum = 0;
 
     public DNIValidator(String dniNumber) {
-        this.dniNumber = dniNumber;
+        if (checkNumberLength(dniNumber) && isNumeric(dniNumber)) {
+            this.dniNumber = dniNumber;
+            isValid = true;
+        } else {
+            System.out.println("Invalid DNI number.");
+            this.dniNumber = null;
+        }
+    }
+
+    private boolean checkNumberLength(String num){
+        return num.length() == DNI_NUMBER_LENGTH;
+    }
+
+    private boolean isNumeric(String num){
+        try {
+            Double.parseDouble(num);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    private int getAlgorithmNum(){
+        return Integer.parseInt(dniNumber) % DNI_DIVISOR_NUM;
+    }
+
+    public boolean isValid(){
+        return isValid;
     }
 
     public DNIValidator() {
@@ -18,9 +47,5 @@ public class DNIValidator {
     public char getLetter(){
         dniAlgorithmNum = getAlgorithmNum();
         return DNI_CORRESPONDING_LETTER.getLetter(dniAlgorithmNum);
-    }
-
-    private int getAlgorithmNum(){
-        return Integer.parseInt(dniNumber) % DNI_DIVISOR_NUM;
     }
 }
--- src/test/java/cd/dperez/DNIValidatorTest.java
+++ src/test/java/cd/dperez/DNIValidatorTest.java
@@ -2,7 +2,7 @@ package cd.dperez;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DNIValidatorTest {
 
@@ -12,6 +12,14 @@ public class DNIValidatorTest {
     public void validDNINumberTest(){
         dniValidator = new DNIValidator("54429671");
         assertEquals('X', dniValidator.getLetter());
+    }
+
+    @Test
+    public void invalidDNINumberTest(){
+        dniValidator = new DNIValidator("1234567");
+        assertFalse(dniValidator.isValid());
+        dniValidator = new DNIValidator("1234g678");
+        assertFalse(dniValidator.isValid());
     }
 
 }
