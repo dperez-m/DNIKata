01d4a14b1ed4ceff7a9ae4e9e1f13223412d3f69
Dani <perdan90@gmail.com>
2024-02-23 16:58:28 +0100

    refactor(DNIValidator): comply with coding conventions

--- src/main/java/cd/dperez/DNIValidator.java
+++ src/main/java/cd/dperez/DNIValidator.java
@@ -2,12 +2,11 @@ package cd.dperez;
 
 public class DNIValidator {
 
-    private final DniLetterDictionary DNI_CORRESPONDING_LETTER = new DniLetterDictionary();
-    private final int DNI_DIVISOR_NUM = 23;
-    private final int DNI_NUMBER_LENGTH = 8;
+    private static final DniLetterDictionary DNI_CORRESPONDING_LETTER = new DniLetterDictionary();
+    private static final int DNI_DIVISOR_NUM = 23;
+    private static final int DNI_NUMBER_LENGTH = 8;
     private boolean isValid = false;
     private String dniNumber = "";
-    private int dniAlgorithmNum = 0;
 
     public DNIValidator(String dniNumber) {
         if (checkNumberLength(dniNumber) && isNumeric(dniNumber)) {
@@ -17,6 +16,9 @@ public class DNIValidator {
             System.out.println("Invalid DNI number.");
             this.dniNumber = null;
         }
+    }
+    public DNIValidator() {
+        dniNumber = "";
     }
 
     private boolean checkNumberLength(String num){
@@ -39,13 +41,9 @@ public class DNIValidator {
     public boolean isValid(){
         return isValid;
     }
-
-    public DNIValidator() {
-        dniNumber = "";
-    }
-
+    
     public char getLetter(){
-        dniAlgorithmNum = getAlgorithmNum();
+        int dniAlgorithmNum = getAlgorithmNum();
         return DNI_CORRESPONDING_LETTER.getLetter(dniAlgorithmNum);
     }
 }
